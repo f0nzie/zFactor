fed5fbb898588ca932ff4b89a6c91ee5836097c8
f0nzie <f0nzie@oilgainsanalytics.com>
2017-06-18 22:55:41 -0500

    move files that ar eno part of the core of the packages to ignored folders

---
+++ R/data.R
@@ -0,0 +1,72 @@
+#' Example 34 fro McCain chapter 03
+#'
+#' @keywords datasets
+#' @docType data
+"ex34"
+
+#' Example 35 from McCain chapter 03
+#'
+#' @keywords datasets
+#' @docType data
+"ex35"
+
+#' Example 35 from McCain chapter 03
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_105"
+
+
+
+#' Example 35 from McCain chapter 03
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_13"
+
+#' Data read from Standing-Katz chart at Tpr = 1.5
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_15"
+
+
+#' Data read from Standing-Katz chart at Tpr = 1.7
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_17"
+
+
+#' Data read from Standing-Katz chart at Tpr = 2.0
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_20"
+
+
+#' Data read from Standing-Katz chart at Tpr = 2.4
+#'
+#' @keywords datasets
+#' @docType data
+"tpr_240"
+
+#' Hal;l-Yarborough tidy dataset
+#'
+#' @keywords datasets
+#' @docType data
+"z_hy_deriv"
+
+#' Hal;l-Yarborough tidy dataset
+#'
+#' @keywords datasets
+#' @docType data
+"z_sk_chart"
+
+#' Hall-Yarborough tidy dataset
+#'
+#' 28 observations of 5 variables
+#'
+#' @keywords datasets
+#' @docType data
+"z_HY"
