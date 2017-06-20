77908905a2b9c90ea65c73b73c9d25918229ec19
f0nzie <f0nzie@oilgainsanalytics.com>
2017-06-20 15:38:18 -0500

    remove primitive function. Replace for a more generic one.

--- R/Standing-Katz.R
+++ R/Standing-Katz.R
@@ -3,46 +3,6 @@
 #'
 #' Read a .txt file that was created from readings of the Standing-Katz chart,
 #' then convert it to a .rda file and plot the curve for given Tpr
-#' @param dfile the text file with SK chart values of Ppr vs z
-#' @param tolerance rounding tolerance to avoid rounding readings that are in
-#' the middle of the grid. "tolerance" adds flexibility in deciding point closeness.
-#' @param save flag to indicate if the .rda file will be saved to disk
-#' @importFrom graphics lines plot
-#' @export
-getZcurve2 <- function(dfile, tolerance = 0.01, save = TRUE) {
-    # Read digitized data from Standing-Katz chart, plot it
-    # and put it in a .rda file
-    # x: Ppr
-    # y: z
-    extdata <- system.file("extdata", package = "zFactor")
-    isNear <- function(n) abs(n - round(n, 1)) <= tolerance
-
-    .tpr <- tools::file_path_sans_ext(dfile)
-    data_file <- paste(extdata, dfile, sep = "/")
-    # add modification "assign(.tpr, tpr)" to store modified dataframe
-    assign(.tpr, utils::read.table(data_file, header = TRUE))  # name same as file name
-    tpr <- get(.tpr)               # get the object
-    colnames(tpr)<- c("Ppr", "z")
-    tpr <- tpr[order(tpr$Ppr),]            # sort Ppr
-    tpr$isNear <- isNear(tpr$Ppr)          # round to nearest Ppr
-    tpr$Ppr_near <- ifelse(tpr$isNear, round(tpr$Ppr/.1)*.1, tpr$Ppr)
-    tpr$diff <- tpr$Ppr - tpr$Ppr_near     # find the difference to nearest
-    assign(.tpr, tpr)
-    if (save) {
-        rda_file <- paste(tools::file_path_sans_ext(dfile), "rda", sep = ".")
-        # rda_file <- paste(getwd(), rda_file, sep = "/")
-        save(list = .tpr, file = rda_file)     # save with same name as input
-    }
-    plot(x = tpr$Ppr, y = tpr$z)                      # as read from SK chart
-    lines(x = tpr$Ppr_near, y = tpr$z, col = "blue")  # nearest rounded points
-}
-
-
-
-#' Read file with readings from Standing-Katz chart, create data file and plot
-#'
-#' Read a .txt file that was created from readings of the Standing-Katz chart,
-#' then convert it to a .rda file and plot the curve for given Tpr
 #' @param tpr Pseudo-reduced temperature curve in SK chart
 #' @param pprRange Takes one of two values: "lp": low pressure, or "hp" for
 #' high pressure
@@ -99,7 +59,7 @@ getStandingKatzCurve <- function(tpr = 1.3, pprRange = "lp",
     View(get(ds_name), title = ds_name)
 }
 
-#' List all Standing-Katz curves available
+#' List all Standing-Katz curves available at Low and High pressures
 #'
 #' @param pprRange Takes one of three values: "lp": low pressure, or "hp" for
 #' high pressure, or 'all' for all the curve files
