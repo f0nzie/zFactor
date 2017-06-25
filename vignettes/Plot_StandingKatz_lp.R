221b9436295afa877efb081113294b9971cef6c0
f0nzie <f0nzie@oilgainsanalytics.com>
2017-06-24 21:32:56 -0500

    improve vignettes

--- vignettes/Plot_StandingKatz_lp.R
+++ vignettes/Plot_StandingKatz_lp.R
@@ -1,19 +1,16 @@
 ## ----setup, include=F, error=T, message=F, warning=F---------------------
-knitr::opts_chunk$set(echo=T, comment=NA, error=T, warning=F, message = F, fig.align = 'center')
+knitr::opts_chunk$set(echo=T, comment=NA, error=T, warning=F, message = F, fig.align = 'center', results = "hold")
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-
 sk <- getStandingKatzCurve()
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-
 getStandingKatzCurve(tpr = 1.05, toSave = FALSE, toView = FALSE)
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-
 tpr_vec <- c(1.05, 1.1)
 result <- getStandingKatzCurve(tpr = tpr_vec, toSave = FALSE, toView = FALSE)
 
@@ -22,15 +19,15 @@ class(result)      # class of the object `result` is a list of dataframes
 names(result)      # name of each dataframe within the list
 
 ## ------------------------------------------------------------------------
-result["1.05"]
+library(tibble)
+as.tibble(result[["1.05"]])
 
 ## ------------------------------------------------------------------------
-result["1.1"]
+as_tibble(result[["1.1"]])
 
 ## ------------------------------------------------------------------------
 # view all the `Tpr` SK individual charts
 tpr_vec <- getCurvesDigitized(pprRange = "lp")
-
 result <- getStandingKatzCurve(tpr_vec, toSave = FALSE,  toView = FALSE)
 
 ## ------------------------------------------------------------------------
@@ -49,17 +46,16 @@ all_tpr2 <- getStandingKatzData(tpr_vec)
 names(all_tpr2)
 
 ## ------------------------------------------------------------------------
-all_tpr2["1.5"]
+library(tibble)
+as.tibble(all_tpr2[["1.5"]])
 
 ## ------------------------------------------------------------------------
 library(data.table)
+library(ggplot2)
 
 # join the dataframes with rbindlist adding an identifier column
 all_tpr_df <- data.table::rbindlist(all_tpr2, idcol = TRUE)
 colnames(all_tpr_df)[1] <- "Tpr"    # name the identifier as Tpr
-
-## ------------------------------------------------------------------------
-library(ggplot2)
 
 ggplot(all_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) + 
     geom_line() +
@@ -78,10 +74,8 @@ tpr_vec <- c(1.05, 1.1, 1.2, 1.3, 1.5, 1.6, 1.7, 1.9, 2.0, 2.4, 2.6, 2.8, 3.0)
 all_tpr2 <- (lapply(tpr_vec, function(x) getStandingKatzData(tpr = x)))
 names(all_tpr2) <- tpr_vec
 
-
 all_tpr_df <- data.table::rbindlist(all_tpr2, idcol = TRUE)
 colnames(all_tpr_df)[1] <- "Tpr"
-
 
 ggplot(all_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) + 
     geom_line() +
@@ -100,10 +94,8 @@ low_tpr_vec <- c(1.05, 1.1, 1.2, 1.3, 1.4)
 low_tpr_li <- (lapply(low_tpr_vec, function(x) getStandingKatzData(tpr = x)))
 names(low_tpr_li) <- low_tpr_vec
 
-
 low_tpr_df <- data.table::rbindlist(low_tpr_li, idcol = TRUE)
 colnames(low_tpr_df)[1] <- "Tpr"
-
 
 ggplot(low_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) + 
     geom_line() +
@@ -122,10 +114,8 @@ med_tpr_vec <- c(1.5, 1.6, 1.7, 1.8, 1.9)
 med_tpr_li <- (lapply(med_tpr_vec, function(x) getStandingKatzData(tpr = x)))
 names(med_tpr_li) <- med_tpr_vec
 
-
 med_tpr_df <- data.table::rbindlist(med_tpr_li, idcol = TRUE)
 colnames(med_tpr_df)[1] <- "Tpr"
-
 
 ggplot(med_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) + 
     geom_line() +
@@ -144,10 +134,8 @@ hi_tpr_vec <- c(2.0, 2.4, 2.6, 2.8, 3.0)
 hi_tpr_li <- (lapply(hi_tpr_vec, function(x) getStandingKatzData(tpr = x)))
 names(hi_tpr_li) <- hi_tpr_vec
 
-
 hi_tpr_df <- data.table::rbindlist(hi_tpr_li, idcol = TRUE)
 colnames(hi_tpr_df)[1] <- "Tpr"
-
 
 ggplot(hi_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) + 
     geom_line() +
@@ -156,6 +144,11 @@ ggplot(hi_tpr_df, aes(x=Ppr, y=z, group=Tpr, color=Tpr)) +
 ## ------------------------------------------------------------------------
 hi_tpr_vec <- c(2.0, 2.4, 2.6, 2.8, 3.0)
 multiplotStandingKatz(hi_tpr_vec)
+
+## ------------------------------------------------------------------------
+library(tibble)
+
+as.tibble(hi_tpr_df)
 
 ## ------------------------------------------------------------------------
 summary(hi_tpr_df)
