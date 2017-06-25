a79ea6f0a41d1a93c93db9f8e315b730177e7a52
f0nzie <f0nzie@oilgainsanalytics.com>
2017-06-25 14:05:13 -0500

    comment lines that are not working with rJava

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,7 @@
-.onLoad <- function(libname, pkgname) {
-    # rJava::.jpackage(pkgname, lib.loc = libname)
-    # print(rJava::.jclassPath())
-}
+# .onLoad doesn't really load rJava.
+# will have to do it from the function itself
+
+# .onLoad <- function(libname, pkgname) {
+#     # rJava::.jpackage(pkgname, lib.loc = libname)
+#     # print(rJava::.jclassPath())
+# }
