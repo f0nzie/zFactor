9aaa4eaadedb2c862635d640d7d5956846efcd7f
f0nzie <f0nzie@oilgainsanalytics.com>
2017-06-27 18:31:17 -0500

    fixing problem with position_dodge overlapping

--- vignettes/Dranchuk-Purvis-Robinson.R
+++ vignettes/Dranchuk-Purvis-Robinson.R
@@ -2,123 +2,118 @@
 knitr::opts_chunk$set(echo=T, comment=NA, error=T, warning=F, message = F, fig.align = 'center', results="hold")
 
 ## ------------------------------------------------------------------------
-# get a z value using DPR correlation
+# get a z value
 library(zFactor)
 
-z.DranchukPurvisRobinson(pres.pr = 1.5, temp.pr = 2.0)
-# HY = 0.9580002
+ppr <- 1.5
+tpr <- 2.0
 
-## ------------------------------------------------------------------------
+z.calc <- z.DranchukPurvisRobinson(pres.pr = ppr, temp.pr = tpr)
+
 # get a z value from the SK chart at the same Ppr and Tpr
-library(zFactor)
 
-tpr_vec <- c(2.0)
-getStandingKatzMatrix(tpr_vector = tpr_vec, 
-                      pprRange = "lp")[1, "1.5"]
+z.chart <- getStandingKatzMatrix(tpr_vector = tpr, 
+                      pprRange = "lp")[1, as.character(ppr)]
+
+
+# calculate the APE
+ape <- abs((z.calc - z.chart) / z.chart) * 100
+
+df <- as.data.frame(list(Ppr = ppr,  z.calc =z.calc, z.chart = z.chart, ape=ape))
+rownames(df) <- tpr
+df
+# HY = 0.9580002; # DAK = 0.9551087
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-z.DranchukPurvisRobinson(pres.pr = 1.5, temp.pr = 1.1)
 
-## ------------------------------------------------------------------------
-library(zFactor)
-tpr_vec <- c(1.1)
-getStandingKatzMatrix(tpr_vector = tpr_vec, 
-                      pprRange = "lp")[1, "1.5"]
+ppr <- 1.5
+tpr <- 1.1
+z.calc <- z.DranchukPurvisRobinson(pres.pr = ppr, temp.pr = tpr)
+
+# From the Standing-Katz chart we obtain a digitized point:
+z.chart <- getStandingKatzMatrix(tpr_vector = tpr, 
+                      pprRange = "lp")[1, as.character(ppr)]
+
+# calculate the APE
+ape <- abs((z.calc - z.chart) / z.chart) * 100
+
+df <- as.data.frame(list(Ppr = ppr,  z.calc =z.calc, z.chart = z.chart, ape=ape))
+rownames(df) <- tpr
+df
 
 ## ------------------------------------------------------------------------
 # test HY with 1st-derivative using the values from paper 
- 
 ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5) 
-tpr <- c(1.3, 1.5, 1.7, 2) 
+tpr <- c(1.05, 1.1, 1.7, 2) 
+
+# calculate using the correlation
+z.calc <- z.DranchukPurvisRobinson(ppr, tpr)
+
+# With the same ppr and tpr vector, we do the same for the Standing-Katz chart
+z.chart <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
+ape <- abs((z.calc - z.chart) / z.chart) * 100
+
+# calculate the APE
+cat("z.correlation \n"); print(z.calc)
+cat("\n z.chart \n"); print(z.chart)
+cat("\n APE \n"); print(ape)
  
-dpr <- z.DranchukPurvisRobinson(pres.pr = ppr, temp.pr = tpr)
-print(dpr)
 
-# From Hall-Yarborough
-#    0.5       1.5       2.5       3.5       4.5       5.5       6.5
-# 1.3 0.9176300 0.7534433 0.6399020 0.6323003 0.6881127 0.7651710 0.8493794
-# 1.5 0.9496855 0.8581232 0.7924067 0.7687902 0.7868071 0.8316848 0.8906351
-# 1.7 0.9682547 0.9134862 0.8756412 0.8605668 0.8694525 0.8978885 0.9396353
-# 2   0.9838234 0.9580002 0.9426939 0.9396286 0.9490995 0.9697839 0.9994317
-
-# From Dranchuk-AbouKassem
-#  0.5       1.5       2.5       3.5       4.5       5.5       6.5
-# 1.3 0.9203019 0.7543694 0.6377871 0.6339357 0.6898314 0.7663247 0.8499523
-# 1.5 0.9509373 0.8593144 0.7929993 0.7710525 0.7896224 0.8331893 0.8904317
-# 1.7 0.9681353 0.9128087 0.8753784 0.8619509 0.8721085 0.9003962 0.9409634
-# 2   0.9824731 0.9551087 0.9400752 0.9385273 0.9497137 0.9715388 1.0015560
+## ------------------------------------------------------------------------
+sum_t_ape <- summary(t(ape))
+sum_t_ape
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-
-sk <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
-print(sk)
-
-## ------------------------------------------------------------------------
-err <- round((sk - dpr) / sk * 100, 2)
-err
-
-# DAK
-# 0.5   1.5  2.5   3.5   4.5   5.5   6.5
-# 1.30 -0.47  0.22 0.03 -0.15 -0.85 -0.97 -0.71
-# 1.50 -0.31 -0.04 0.13 -0.14  0.05  0.34  0.18
-# 1.70 -0.01  0.13 0.07 -0.58 -0.94 -0.38  0.11
-# 2.00 -0.05  0.09 0.10 -0.16 -0.50 -0.26  0.14
-
-## ------------------------------------------------------------------------
-print(colSums(err))
-
-## ------------------------------------------------------------------------
-print(rowSums(err))
-
-## ------------------------------------------------------------------------
-library(zFactor)
-
-tpr2 <- c(1.05, 1.1) 
+# enter vectors for Tpr and Ppr
+tpr2 <- c(1.2, 1.3, 1.5, 2.0, 3.0) 
 ppr2 <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5) 
+# get z values from the SK chart
+z.chart <- getStandingKatzMatrix(ppr_vector = ppr2, tpr_vector = tpr2, pprRange = "lp")
 
-sk2 <- getStandingKatzMatrix(ppr_vector = ppr2, tpr_vector = tpr2, pprRange = "lp")
-sk2
-
-## ------------------------------------------------------------------------
+# We do the same with the HY correlation:
 # calculate z values at lower values of Tpr
-library(zFactor)
+z.calc <- z.DranchukPurvisRobinson(pres.pr = ppr2, temp.pr = tpr2) 
+ape <- abs((z.calc - z.chart) / z.chart) * 100
 
-dpr2 <- z.DranchukPurvisRobinson(pres.pr = ppr2, temp.pr = tpr2)
-print(dpr2)
-
-## ------------------------------------------------------------------------
-err2 <- round((sk2 - dpr2) / sk2 * 100, 2)
-err2
-
-# DAK
-# 0.5    1.5    2.5   3.5   4.5   5.5
-# 1.05 -0.13 -12.15 -12.78 -7.49 -4.34 -1.68
-# 1.10 -0.36  -4.79  -4.97 -3.56 -2.14 -1.21
+# calculate the APE
+cat("z.correlation \n"); print(z.calc)
+cat("\n z.chart \n"); print(z.chart)
+cat("\n APE \n"); print(ape)
 
 ## ------------------------------------------------------------------------
-t_err2 <- t(err2)
-t_err2
-
-## ------------------------------------------------------------------------
-sum_t_err2 <- summary(t_err2)
-sum_t_err2
+sum_t_ape <- summary(t(ape))
+sum_t_ape
+ # Hall-Yarborough
+ #      1.2               1.3              1.5               2         
+ # Min.   :0.05224   Min.   :0.1105   Min.   :0.1021   Min.   :0.0809  
+ # 1st Qu.:0.09039   1st Qu.:0.2080   1st Qu.:0.1623   1st Qu.:0.1814  
+ # Median :0.28057   Median :0.3181   Median :0.1892   Median :0.1975  
+ # Mean   :0.30122   Mean   :0.3899   Mean   :0.2597   Mean   :0.2284  
+ # 3rd Qu.:0.51710   3rd Qu.:0.5355   3rd Qu.:0.3533   3rd Qu.:0.2627  
+ # Max.   :0.57098   Max.   :0.8131   Max.   :0.5162   Max.   :0.4338  
+ #       3          
+ # Min.   :0.09128  
+ # 1st Qu.:0.17466  
+ # Median :0.35252  
+ # Mean   :0.34820  
+ # 3rd Qu.:0.52184  
+ # Max.   :0.59923  
 
 ## ------------------------------------------------------------------------
 library(zFactor)
 library(tibble)
+library(ggplot2)
 
 tpr2 <- c(1.05, 1.1, 1.2, 1.3) 
 ppr2 <- c(0.5, 1.0, 1.5, 2, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5) 
 
-sk_dpr_2 <- createTidyFromMatrix(ppr2, tpr2, correlation = "DPR")
-as.tibble(sk_dpr_2)
+sk_corr_2 <- createTidyFromMatrix(ppr2, tpr2, correlation = "DPR")
+as.tibble(sk_corr_2)
 
-## ------------------------------------------------------------------------
-library(ggplot2)
 
-p <- ggplot(sk_dpr_2, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
+p <- ggplot(sk_corr_2, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
     geom_line() +
     geom_point() +
     geom_errorbar(aes(ymin=z.calc-dif, ymax=z.calc+dif), width=.4,
@@ -126,28 +121,18 @@ p <- ggplot(sk_dpr_2, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
 print(p)
 
 ## ------------------------------------------------------------------------
-sk_dpr_3 <- sk_dpr_2[sk_dpr_2$Tpr==1.05,]
-sk_dpr_3
+library(zFactor)
 
-## ------------------------------------------------------------------------
+sk_corr_3 <- sk_corr_2[sk_corr_2$Tpr==1.05,]
+sk_corr_3
 
-p <- ggplot(sk_dpr_3, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
+
+p <- ggplot(sk_corr_3, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
     geom_line() +
     geom_point() +
-    geom_errorbar(aes(ymin=z.calc-dif, ymax=z.calc+dif), width=.4,
+    geom_errorbar(aes(ymin=z.calc-dif, ymax=z.calc+dif), width=.2,
                   position=position_dodge(0.05))
 print(p)
-
-## ------------------------------------------------------------------------
-summary(sk_dpr_3)
-
- #         dif DAK      
- # Min.   :-0.048404  
- # 1st Qu.:-0.035300  
- # Median :-0.025978  
- # Mean   :-0.023178  
- # 3rd Qu.:-0.009960  
- # Max.   : 0.002325
 
 ## ------------------------------------------------------------------------
 library(ggplot2)
@@ -168,51 +153,43 @@ print(p)
 
 ## ------------------------------------------------------------------------
 # MSE: Mean Squared Error
-# RMSE: Root Mean Sqyared Error
+# RMSE: Root Mean Squared Error
 # RSS: residual sum of square
 # ARE:  Average Relative Error, %
 # AARE: Average Absolute Relative Error, %
 library(dplyr)
 grouped <- group_by(sk_corr_all, Tpr, Ppr)
+
 smry_tpr_ppr <- summarise(grouped, 
           RMSE= sqrt(mean((z.chart-z.calc)^2)), 
+          MPE = sum((z.calc - z.chart) / z.chart) * 100 / n(),
+          MAPE = sum(abs((z.calc - z.chart) / z.chart)) * 100 / n(), 
           MSE = sum((z.calc - z.chart)^2) / n(), 
           RSS = sum((z.calc - z.chart)^2),
-          ARE = sum((z.calc - z.chart) / z.chart) * 100 / n(),
-          AARE = sum( abs((z.calc - z.chart) / z.chart)) * 100 / n()
+          MAE = sum(abs(z.calc - z.chart)) / n(),
+          RMLSE = sqrt(1/n()*sum((log(z.calc +1)-log(z.chart +1))^2))
           )
 
 ggplot(smry_tpr_ppr, aes(Ppr, Tpr)) + 
-    geom_tile(data=smry_tpr_ppr, aes(fill=AARE), color="white") +
+    geom_tile(data=smry_tpr_ppr, aes(fill=MAPE), color="white") +
     scale_fill_gradient2(low="blue", high="red", mid="yellow", na.value = "pink",
-                         midpoint=12.5, limit=c(0, 25), name="AARE") + 
+                         midpoint=12.5, limit=c(0, 25), name="MAPE") + 
     theme(axis.text.x = element_text(angle=45, vjust=1, size=11, hjust=1)) + 
     coord_equal() +
     ggtitle("Dranchuk-Purvis-Robinson", subtitle = "DPR")
 
-## ----results="hold"------------------------------------------------------
-# get all `lp` Tpr curves
-tpr <- getCurvesDigitized(pprRange = "lp")
-ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5) 
+## ------------------------------------------------------------------------
+library(dplyr)
 
-# calculate HY for the given Tpr
-all_dak <- z.DranchukPurvisRobinson(pres.pr = ppr, temp.pr = tpr)
-cat("Calculated from the correlation \n")
-print(all_dak) 
+sk_corr_all %>%
+    filter(Tpr %in% c("1.05", "1.1")) %>%
+    ggplot(aes(x = z.chart, y=z.calc, group = Tpr, color = Tpr)) +
+    geom_point(size = 3) +
+    geom_line(aes(x = z.chart, y = z.chart), color = "black") +
+    facet_grid(. ~ Tpr) +
+    geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)), 
+                  position=position_dodge(0.5))
 
-cat("\nStanding-Katz chart\n")
-all_sk <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
-all_sk
-
-# find the error
-cat("\n Errors in percentage \n")
-all_err <- round((all_sk - all_dak) / all_sk * 100, 2)  # in percentage
-all_err
-
-cat("\n Errors in Ppr\n")
-summary(all_err)
-
-# for the transposed matrix
-cat("\n Errors for the transposed matrix: Tpr \n")
-summary(t(all_err))
+## ------------------------------------------------------------------------
+as.tibble(smry_tpr_ppr)
 
--- vignettes/Hall-Yarborough.R
+++ vignettes/Hall-Yarborough.R
@@ -5,115 +5,97 @@ knitr::opts_chunk$set(echo=T, comment=NA, error=T, warning=F, message = F, fig.a
 # get a z value using HY
 library(zFactor)
 
-z.HallYarborough(pres.pr = 1.5, temp.pr = 2.0)
+ppr <- 1.5
+tpr <- 2.0
+z.calc <- z.HallYarborough(pres.pr = ppr, temp.pr = tpr)
 
-## ------------------------------------------------------------------------
 # get a z value from the SK chart at the same Ppr and Tpr
-library(zFactor)
+z.chart <- getStandingKatzMatrix(tpr_vector = tpr, 
+                      pprRange = "lp")[1, as.character(ppr)]
 
-tpr_vec <- c(2.0)
-getStandingKatzMatrix(tpr_vector = tpr_vec, 
-                      pprRange = "lp")[1, "1.5"]
+# calculate the APE
+ape <- abs((z.calc - z.chart) / z.chart) * 100
+
+df <- as.data.frame(list(Ppr = ppr,  z.calc =z.calc, z.chart = z.chart, ape=ape))
+rownames(df) <- tpr
+df
 
 ## ------------------------------------------------------------------------
 library(zFactor)
+ppr <- 1.5
+tpr <- 1.1
+z.calc <- z.HallYarborough(pres.pr = ppr, temp.pr = tpr)
 
-z.HallYarborough(pres.pr = 1.5, temp.pr = 1.1)
+# From the Standing-Katz chart we obtain a digitized point:
+z.chart <- getStandingKatzMatrix(tpr_vector = tpr, 
+                      pprRange = "lp")[1, as.character(ppr)]
 
-## ------------------------------------------------------------------------
-library(zFactor)
+# calculate the APE
+ape <- abs((z.calc - z.chart) / z.chart) * 100
 
-tpr_vec <- c(1.1)
-getStandingKatzMatrix(tpr_vector = tpr_vec, 
-                      pprRange = "lp")[1, "1.5"]
+df <- as.data.frame(list(Ppr = ppr,  z.calc =z.calc, z.chart = z.chart, ape=ape))
+rownames(df) <- tpr
+df
 
 ## ------------------------------------------------------------------------
 library(zFactor)
 
 ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5) 
-tpr <- c(1.3, 1.5, 1.7, 2) 
+tpr <- c(1.05, 1.1, 1.7, 2) 
 
-z.HallYarborough(ppr, tpr)
+# calculate using the correlation
+z.calc <- z.HallYarborough(ppr, tpr)
 
-## ------------------------------------------------------------------------
-# test HY with 1st-derivative using the values from paper 
- 
-ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5) 
-tpr <- c(1.3, 1.5, 1.7, 2) 
- 
-hy <- sapply(ppr, function(x)  
-    sapply(tpr, function(y) zFactor:::.z.HallYarborough(pres.pr = x, temp.pr = y))) 
- 
-rownames(hy) <- tpr 
-colnames(hy) <- ppr 
-print(hy) 
+# With the same ppr and tpr vector, we do the same for the Standing-Katz chart
+z.chart <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
+ape <- abs((z.calc - z.chart) / z.chart) * 100
+
+# calculate the APE
+cat("z.correlation \n"); print(z.calc)
+cat("\n z.chart \n"); print(z.chart)
+cat("\n APE \n"); print(ape)
 
 ## ------------------------------------------------------------------------
-library(zFactor)
-
-sk <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
-sk
-
-## ------------------------------------------------------------------------
-err <- round((sk - hy) / sk * 100, 2)
-err
-
-## ------------------------------------------------------------------------
-print(colSums(err))
-
-## ------------------------------------------------------------------------
-print(rowSums(err))
+sum_t_ape <- summary(t(ape))
+sum_t_ape
 
 ## ------------------------------------------------------------------------
 library(zFactor)
-
-tpr2 <- c(1.05, 1.1) 
+# enter vectors for Tpr and Ppr
+tpr2 <- c(1.2, 1.3, 1.5, 2.0, 3.0) 
 ppr2 <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5) 
+# get z values from the SK chart
+z.chart <- getStandingKatzMatrix(ppr_vector = ppr2, tpr_vector = tpr2, pprRange = "lp")
 
-
-sk2 <- getStandingKatzMatrix(ppr_vector = ppr2, tpr_vector = tpr2, pprRange = "lp")
-sk2
-
-## ------------------------------------------------------------------------
+# We do the same with the HY correlation:
 # calculate z values at lower values of Tpr
-library(zFactor)
+z.calc <- z.HallYarborough(pres.pr = ppr2, temp.pr = tpr2) 
+ape <- abs((z.calc - z.chart) / z.chart) * 100
 
-tpr <- c(1.05, 1.1)
-ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5) 
-
-hy2 <- z.HallYarborough(pres.pr = ppr, temp.pr = tpr) 
-
-print(hy2)
-
-## ------------------------------------------------------------------------
-err2 <- round((sk2 - hy2) / sk2 * 100, 2)
-err2
+# calculate the APE
+cat("z.correlation \n"); print(z.calc)
+cat("\n z.chart \n"); print(z.chart)
+cat("\n APE \n"); print(ape)
 
 ## ------------------------------------------------------------------------
-t_err2 <- t(err2)
-t_err2
-
-## ------------------------------------------------------------------------
-sum_t_err2 <- summary(t_err2)
-sum_t_err2
+sum_t_ape <- summary(t(ape))
+sum_t_ape
 
 ## ------------------------------------------------------------------------
 library(zFactor)
 library(tibble)
-
-tpr2 <- c(1.05, 1.1, 1.2, 1.3) 
-ppr2 <- c(0.5, 1.0, 1.5, 2, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5) 
-
-sk_hy_2 <- createTidyFromMatrix(ppr2, tpr2, correlation = "HY")
-as.tibble(sk_hy_2)
-
-## ------------------------------------------------------------------------
 library(ggplot2)
 
-p <- ggplot(sk_hy_2, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
+tpr2 <- c(1.05, 1.1, 1.2, 1.3)
+ppr2 <- c(0.5, 1.0, 1.5, 2, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5)
+
+sk_corr_2 <- createTidyFromMatrix(ppr2, tpr2, correlation = "HY")
+as.tibble(sk_corr_2)
+
+p <- ggplot(sk_corr_2, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
     geom_line() +
     geom_point() +
-    geom_errorbar(aes(ymin=z.calc-dif, ymax=z.calc+dif), width=.4,
+    geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)), width=.4,
                   position=position_dodge(0.05))
 print(p)
 
@@ -127,7 +109,6 @@ ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
 sk_corr_all <- createTidyFromMatrix(ppr, tpr_all, correlation = "HY")
 as.tibble(sk_corr_all)
 
-
 p <- ggplot(sk_corr_all, aes(x=Ppr, y=z.calc, group=Tpr, color=Tpr)) +
     geom_line() +
     geom_point() +
@@ -139,22 +120,26 @@ print(p)
 # MSE: Mean Squared Error
 # RMSE: Root Mean Sqyared Error
 # RSS: residual sum of square
-# ARE:  Average Relative Error, %
-# AARE: Average Absolute Relative Error, %
+# RMSLE: Root Mean Squared Logarithmic Error. Penalizes understimation.
+# MAPE: Mean Absolute Percentage Error = AARE
+# MPE: Mean Percentage error = ARE
+# MAE: Mean Absolute Error
 library(dplyr)
 grouped <- group_by(sk_corr_all, Tpr, Ppr)
 smry_tpr_ppr <- summarise(grouped, 
           RMSE= sqrt(mean((z.chart-z.calc)^2)), 
+          MPE = sum((z.calc - z.chart) / z.chart) * 100 / n(),
+          MAPE = sum(abs((z.calc - z.chart) / z.chart)) * 100 / n(), 
           MSE = sum((z.calc - z.chart)^2) / n(), 
           RSS = sum((z.calc - z.chart)^2),
-          ARE = sum((z.calc - z.chart) / z.chart) * 100 / n(),
-          AARE = sum( abs((z.calc - z.chart) / z.chart)) * 100 / n()
+          MAE = sum(abs(z.calc - z.chart)) / n(),
+          RMLSE = sqrt(1/n()*sum((log(z.calc +1)-log(z.chart +1))^2))
           )
 
 ggplot(smry_tpr_ppr, aes(Ppr, Tpr)) + 
-    geom_tile(data=smry_tpr_ppr, aes(fill=AARE), color="white") +
+    geom_tile(data=smry_tpr_ppr, aes(fill=MAPE), color="white") +
     scale_fill_gradient2(low="blue", high="red", mid="yellow", na.value = "pink",
-                         midpoint=12.5, limit=c(0, 25), name="AARE") + 
+                         midpoint=12.5, limit=c(0, 25), name="MAPE") + 
     theme(axis.text.x = element_text(angle=45, vjust=1, size=11, hjust=1)) + 
     coord_equal() +
     ggtitle("Hall-Yarborough", subtitle = "HY")
@@ -162,87 +147,28 @@ ggplot(smry_tpr_ppr, aes(Ppr, Tpr)) +
 ## ------------------------------------------------------------------------
 library(dplyr)
 
-sk_hy_all %>%
-    filter(Tpr %in% c("1.05", "1.1", "1.2", "1.3")) %>%
+sk_corr_all %>%
+    filter(Tpr %in% c("1.05", "1.1")) %>%
     ggplot(aes(x = z.chart, y=z.calc, group = Tpr, color = Tpr)) +
     geom_point(size = 3) +
     geom_line(aes(x = z.chart, y = z.chart), color = "black") +
     facet_grid(. ~ Tpr) +
-    geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)), position=position_dodge(0.5), width = 0.05)
+    geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)),
+                  position=position_dodge(0.05))
 
 ## ------------------------------------------------------------------------
-library(dplyr)
-
-sk_hy_all %>%
-    filter(Tpr %in% c("1.4", "1.5", "1.6", "1.7", "1.8", "1.9")) %>%
-    ggplot(aes(x = z.chart, y=z.calc, group = Tpr, color = Tpr)) +
-    geom_point(size = 3) +
-    geom_line(aes(x = z.chart, y = z.chart), color = "black") +
-    facet_grid(. ~ Tpr) +
-    geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)), position=position_dodge(0.5), width = 0.05)
+as.tibble(smry_tpr_ppr)
 
 ## ------------------------------------------------------------------------
-library(dplyr)
-
-sk_hy_all %>%
-    filter(as.double(Tpr) > 2 ) %>%
-    ggplot(aes(x = z.chart, y=z.calc, group = Tpr, color = Tpr)) +
-    geom_point(size = 3) +
-    geom_line(aes(x = z.chart, y = z.chart), color = "black") +
-    facet_grid(. ~ Tpr) +
-geom_errorbar(aes(ymin=z.calc-abs(dif), ymax=z.calc+abs(dif)), position=position_dodge(0.5), width = 0.05)
-
-## ------------------------------------------------------------------------
-sk_hy_all
-
-## ------------------------------------------------------------------------
-sk_hy_all$z.rng <- ifelse(sk_hy_all$Ppr > 3, "zgt05", "zlt05")
-
-q <- ggplot(sk_hy_all, aes(x=z.chart, y=z.calc)) +
-    geom_line() +
-    geom_point() +
-     facet_grid(Tpr ~ .)
-print(q)
-
-
-## ------------------------------------------------------------------------
-
-
-## ------------------------------------------------------------------------
-
-q <- ggplot(sk_hy_all, aes(x=z.chart, y=z.calc, color=Tpr)) +
-    geom_line() +
-    geom_point() +
-    facet_grid(Tpr ~ .)
-print(q)
-
-
-## ------------------------------------------------------------------------
-# get all `lp` Tpr curves
-tpr <- getCurvesDigitized(pprRange = "lp")
+# test HY with 1st-derivative using the values from paper 
+ 
 ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5) 
-
-# calculate HY for the given Tpr
-all_hy <- sapply(ppr, function(x)  
-    sapply(tpr, function(y) z.HallYarborough(pres.pr = x, temp.pr = y))) 
-rownames(all_hy) <- tpr 
-colnames(all_hy) <- ppr 
-cat("Calculated Hall-Yarborough\n")
-print(all_hy) 
-
-cat("\nStanding-Katz chart\n")
-all_sk <- getStandingKatzMatrix(ppr_vector = ppr, tpr_vector = tpr)
-all_sk
-
-# find the error
-cat("\n Errors in percentage \n")
-all_err <- round((all_sk - all_hy) / all_sk * 100, 2)  # in percentage
-all_err
-
-cat("\n Errors in Ppr\n")
-summary(all_err)
-
-# for the transposed matrix
-cat("\n Errors for the transposed matrix: Tpr \n")
-summary(t(all_err))
+tpr <- c(1.3, 1.5, 1.7, 2) 
+ 
+hy <- sapply(ppr, function(x)  
+    sapply(tpr, function(y) zFactor:::.z.HallYarborough(pres.pr = x, temp.pr = y))) 
+ 
+rownames(hy) <- tpr 
+colnames(hy) <- ppr 
+print(hy) 
 
