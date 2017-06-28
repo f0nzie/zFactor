3a3327028e080b340e91440e1713dffbd9401130
superuser mintkde <mintkde@reyesaguayo.com>
2017-06-28 15:13:49 -0500

    comment line that saves RDA for test of DPR.

--- tests/testthat/test_DranchukPurvisRobinson.R
+++ tests/testthat/test_DranchukPurvisRobinson.R
@@ -10,7 +10,8 @@ expect_equal(z.DranchukPurvisRobinson(0.5, 1.3), 0.9197157, tolerance = 1E-7)
 test_that("DPR corr matches solution of 4x7 Ppr, Tpr matrix", {
     ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
     tpr <- c(1.3, 1.5, 1.7, 2)
-    dpr <- z.DranchukAbuKassem(ppr, tpr); save(dpr, file = "dpr_4x7.rda")
+
+    # dpr <- z.DranchukAbuKassem(ppr, tpr); save(dpr, file = "dpr_4x7.rda")
     load(file = "dpr_4x7.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dpr)
 })
@@ -29,6 +30,7 @@ test_that("DPR corr matches solution of 2x6 Ppr, Tpr matrix", {
 test_that("DPR corr matches solution of 4x13 Ppr, Tpr matrix", {
     tpr <- c(1.05, 1.1, 1.2, 1.3)
     ppr <- c(0.5, 1.0, 1.5, 2, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5)
+
     # dpr <- z.DranchukAbuKassem(ppr, tpr); save(dpr, file = "dpr_4x13.rda")
     load(file = "dpr_4x13.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dpr)
@@ -37,6 +39,7 @@ test_that("DPR corr matches solution of 4x13 Ppr, Tpr matrix", {
 test_that("DPR corr matches solution of 16x7 Ppr, Tpr (all) matrix", {
     tpr <- getCurvesDigitized(pprRange = "lp")
     ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
+
     # dpr <- z.DranchukAbuKassem(ppr, tpr); save(dpr, file = "dpr_16x7.rda")
     load(file = "dpr_16x7.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dpr)
