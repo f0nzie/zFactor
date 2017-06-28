2bd02798eb76c57c1356941060ddb196eed1ce81
superuser mintkde <mintkde@reyesaguayo.com>
2017-06-28 15:13:06 -0500

    comment lines that sve RDA file for test in DAK.

--- tests/testthat/test_DranchukAbuKassem.R
+++ tests/testthat/test_DranchukAbuKassem.R
@@ -37,17 +37,17 @@ test_that("test DAK between 7 Ppr values and 4 Tpr", {
 test_that("DAK corr matches solution of 4x7 Ppr, Tpr matrix", {
     ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
     tpr <- c(1.3, 1.5, 1.7, 2)
-    dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_4x7.rda")
+    # dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_4x7.rda")
     load(file = "dak_4x7.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dak)
 })
 
 
-test_that("HY corr matches solution of 2x6 Ppr, Tpr matrix", {
+test_that("DAK corr matches solution of 2x6 Ppr, Tpr matrix", {
     tpr <- c(1.05, 1.1)
     ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5)
 
-    dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_2x6.rda")
+    # dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_2x6.rda")
     load(file = "dak_2x6.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dak)
 })
@@ -56,7 +56,7 @@ test_that("HY corr matches solution of 2x6 Ppr, Tpr matrix", {
 test_that("DAK corr matches solution of 4x13 Ppr, Tpr matrix", {
     tpr <- c(1.05, 1.1, 1.2, 1.3)
     ppr <- c(0.5, 1.0, 1.5, 2, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5)
-    dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_4x13.rda")
+    # dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_4x13.rda")
     load(file = "dak_4x13.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dak)
 })
@@ -64,7 +64,7 @@ test_that("DAK corr matches solution of 4x13 Ppr, Tpr matrix", {
 test_that("DAK corr matches solution of 16x7 Ppr, Tpr (all) matrix", {
     tpr <- getCurvesDigitized(pprRange = "lp")
     ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
-    dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_16x7.rda")
+    # dak <- z.DranchukAbuKassem(ppr, tpr); save(dak, file = "dak_16x7.rda")
     load(file = "dak_16x7.rda");
     expect_equal(z.DranchukAbuKassem(ppr, tpr), dak)
 })
