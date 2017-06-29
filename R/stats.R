278e8fd368aa3c44c7a7a11c3ffa8d5e6f1e6f50
superuser mintkde <mintkde@reyesaguayo.com>
2017-06-28 23:07:25 -0500

    export ggplot functions in stats.R

---
+++ R/stats.R
@@ -0,0 +1,34 @@
+#' @importFrom dplyr group_by summarise
+z.stats <- function(correlation = "DAK", pprRange = "lp") {
+    Ppr <- NULL; Tpr <- NULL; z.calc <- NULL; z.chart <- NULL; n <- NULL
+    # get all `lp` Tpr curves
+    tpr_all <- getCurvesDigitized(pprRange)
+    ppr <- c(0.5, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.0, 6.5, 7.0)
+    sk_corr_all <- createTidyFromMatrix(ppr, tpr_all, correlation)
+
+    grouped <- group_by(sk_corr_all, Tpr, Ppr)
+    smry_tpr_ppr <- summarise(grouped,
+                              RMSE= sqrt(mean((z.chart-z.calc)^2)),
+                              MPE = sum((z.calc - z.chart) / z.chart) * 100 / n(),
+                              MAPE = sum(abs((z.calc - z.chart) / z.chart)) * 100 / n(),
+                              MSE = sum((z.calc - z.chart)^2) / n(),
+                              RSS = sum((z.calc - z.chart)^2),
+                              MAE = sum(abs(z.calc - z.chart)) / n(),
+                              RMLSE = sqrt(1/n()*sum((log(z.calc +1)-log(z.chart +1))^2))
+    )
+    smry_tpr_ppr
+}
+
+#' @import ggplot2
+z.plot.range <- function(correlation = "DAK", pprRange = "lp") {
+    Ppr <- NULL; Tpr <- NULL; MAPE <- NULL; z.calc <- NULL; z.chart <- NULL
+    smry_tpr_ppr <- z.stats(correlation, pprRange)
+    g <- ggplot(smry_tpr_ppr, aes(Ppr, Tpr))
+    g <- g + geom_tile(data=smry_tpr_ppr, aes(fill=MAPE), color="white") +
+        scale_fill_gradient2(low="blue", high="red", mid="yellow", na.value = "pink",
+                             midpoint=12.5, limit=c(0, 25), name="MAPE") +
+        theme(axis.text.x = element_text(angle=45, vjust=1, size=11, hjust=1)) +
+        coord_equal() +
+        ggtitle("Hall-Yarborough", subtitle = "HY")
+    print(g)
+}
