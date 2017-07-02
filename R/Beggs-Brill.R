4f2c8f654bff637c34a34e38000698ff3ccb0e2d
superuser mintkde <mintkde@reyesaguayo.com>
2017-07-02 18:36:45 -0500

    add examples

--- R/Beggs-Brill.R
+++ R/Beggs-Brill.R
@@ -11,7 +11,13 @@
 #' @param verbose print internal
 #' @rdname Beggs-Brill
 #' @export
-#' @export
+#' @examples
+#' ## one single z calculation
+#' z.BeggsBrill(pres.pr = 1.5, temp.pr = 2.0)
+#' ## calculate z for multiple values of Tpr and Ppr
+#' ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
+#' tpr <- c(1.3, 1.5, 1.7, 2)
+#' z.BeggsBrill(pres.pr = ppr, temp.pr = tpr)
 z.BeggsBrill <- function(pres.pr, temp.pr,
                          tolerance = 1e-13, verbose = FALSE) {
 
