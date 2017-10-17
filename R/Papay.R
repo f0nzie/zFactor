6115abc896eb1b6cc231bddc926caf10ccca5e2c
f0nzie <f0nzie@oilgainsanalytics.com>
2017-10-16 22:08:35 -0500

    z.Papay function implemented and tested with two examples

--- R/Papay.R
+++ R/Papay.R
@@ -1,37 +1,39 @@
 
-#' #' Papay correlation
-#' #'
-#' #' Calculate the Z factor with the Brill-Beggs correlation
-#' #'
-#' #' @param pres.pr pseudo-reduced pressure
-#' #' @param temp.pr pseudo-reduced temperature
-#' #' @param tolerance rounding tolerance to avoid rounding readings that are in
-#' #' the middle of the grid. "tolerance" adds flexibility in deciding point closeness.
-#' #' @param verbose print internal
-#' #' @rdname Beggs-Brill
-#' #' @export
-#' #' @examples
-#' #' ## one single z calculation
-#' #' z.BeggsBrill(pres.pr = 1.5, temp.pr = 2.0)
-#' #' ## calculate z for multiple values of Tpr and Ppr
-#' #' ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
-#' #' tpr <- c(1.3, 1.5, 1.7, 2)
-#' #' z.BeggsBrill(pres.pr = ppr, temp.pr = tpr)
-#' z.BeggsBrill <- function(pres.pr, temp.pr,
-#'                          tolerance = 1e-13, verbose = FALSE) {
-#'     # calls the core function.
-#'     # this function converts the results to a matrix
-#'     co <- sapply(pres.pr, function(x)
-#'         sapply(temp.pr, function(y)
-#'             .z.BeggsBrill(pres.pr = x, temp.pr = y,
-#'                                       tolerance = tolerance, verbose = verbose)))
-#'     if (length(pres.pr) > 1 || length(temp.pr) > 1) {
-#'         co <- matrix(co, nrow = length(temp.pr), ncol = length(pres.pr))
-#'         rownames(co) <- temp.pr
-#'         colnames(co) <- pres.pr
-#'     }
-#'     return(co)
-#' }
+#' Papay correlation
+#'
+#' Calculate the Z factor with the Papay's correlation
+#'
+#' @param pres.pr pseudo-reduced pressure
+#' @param temp.pr pseudo-reduced temperature
+#' @param tolerance rounding tolerance to avoid rounding readings that are in
+#' the middle of the grid. "tolerance" adds flexibility in deciding point closeness.
+#' @param verbose print internal
+#' @rdname Papay
+#' @export
+#' @examples
+#' # Example 1
+#' ## one single z calculation
+#' z.Papay(pres.pr = 1.5, temp.pr = 2.0)
+#' # Example 2
+#' ## calculate z for multiple values of Tpr and Ppr
+#' ppr <- c(0.5, 1.5, 2.5, 3.5, 4.5, 5.5, 6.5)
+#' tpr <- c(1.3, 1.5, 1.7, 2)
+#' z.Papay(pres.pr = ppr, temp.pr = tpr)
+z.Papay <- function(pres.pr, temp.pr,
+                         tolerance = 1e-13, verbose = FALSE) {
+    # calls the core function.
+    # this function converts the results to a matrix
+    co <- sapply(pres.pr, function(x)
+        sapply(temp.pr, function(y)
+            .z.Papay(pres.pr = x, temp.pr = y,
+                                      tolerance = tolerance, verbose = verbose)))
+    if (length(pres.pr) > 1 || length(temp.pr) > 1) {
+        co <- matrix(co, nrow = length(temp.pr), ncol = length(pres.pr))
+        rownames(co) <- temp.pr
+        colnames(co) <- pres.pr
+    }
+    return(co)
+}
 
 
 .z.Papay <- function(pres.pr, temp.pr,
