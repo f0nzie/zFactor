c2360881ddc701a0a25bfdbf0fdcdf9bf88cfe01
fonzie zbook <alfonso.r.reyes@gmail.com>
2017-10-19 12:24:58 -0500

    add unit test for stats.R. testing z_correlations object. added test for z.stats

---
+++ tests/testthat/test_stats.R
@@ -0,0 +1,47 @@
+library(testthat)
+
+
+# context("z.stats")
+#
+# test_that("z.stats generates same dataframe size for all correlations", {
+#     sh_corrs <- (zFactor:::z_correlations$short)
+#     mean_mape <- vector("numeric")
+#     for (corr in sh_corrs) {
+#         expect_equal(dim(z.stats(corr)), c(112, 8))
+#         mape <- z.stats(corr)$MAPE
+#         # print(corr); print(max(mape)); print(min(mape)); print(mean(mape))
+#         mean_mape <- c(mean_mape, mean(mape))
+#
+#     }
+#     names(mean_mape) <- sh_corrs
+#     # row.names(mean_mape) <- "MAPE"
+#     # print(z.stats(corr))
+#     # mape <- z.stats(corr)$MAPE
+#     # print(max(mape)) # 85.48267
+#     # print(min(mape)) # 0.000507908
+#     # print(median(mape)) # 0.000507908
+#     # print(Mode(mape)) # 0.000507908
+#     # print(fivenum(mape)) # 0.000507908
+#     print(mean_mape)
+# })
+
+
+
+
+context("z_correlations")
+
+test_that("z_correlations matches the short names of the correlations", {
+    sh_corrs <- (zFactor:::z_correlations$long)
+    for (corr in sh_corrs) {
+        expect_true(corr %in% c("Beggs-Brill", "Hall-Yarborough", "Dranchuk-AbuKassem",
+                                "Dranchuk-Purvis-Robinson", "Shell", "Ann10", "Papp"))
+    }
+})
+
+
+test_that("z_correlations matches the short names of the correlations", {
+    sh_corrs <- (zFactor:::z_correlations$short)
+    for (corr in sh_corrs) {
+        expect_true(corr %in% c("BB", "HY", "DAK", "DPR", "SH", "N10", "PP"))
+    }
+})
