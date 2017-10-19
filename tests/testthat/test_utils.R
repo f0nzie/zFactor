58e8d53aa1f6080b8bfd42f52858744434fa9a14
fonzie zbook <alfonso.r.reyes@gmail.com>
2017-10-18 23:04:09 -0500

    add tests for get_z_correlations()

---
+++ tests/testthat/test_utils.R
@@ -0,0 +1,41 @@
+library(testthat)
+
+context("utils")
+
+test_that("get_z_correlations return short", {
+    short = c("BB",  "HY",  "DAK", "DPR", "SH",  "N10", "PP")
+    expect_equal(get_z_correlations(how = "short"), short)
+    # test default
+    expect_equal(get_z_correlations(), short)
+})
+
+
+test_that("get_z_correlations return long", {
+    long <- c("Beggs-Brill", "Hall-Yarborough",  "Dranchuk-AbuKassem" ,
+              "Dranchuk-Purvis-Robinson", "Shell", "Ann10",  "Papp")
+    expect_equal(get_z_correlations(how = "long"), long)
+    # print(get_z_correlations(how = "long"))
+})
+
+test_that("get_z_correlations return function", {
+    function.names <- c("z.BeggsBrill", "z.HallYarborough", "z.DranchukAbuKassem",
+                        "z.DranchukPurvisRobinson", "z.Shell", "z.Ann10", "z.Papp")
+    expect_equal(get_z_correlations(how = "function"), function.names)
+    # print(get_z_correlations(how = "function"))
+})
+
+test_that("get_z_correlations return wrong parameter", {
+    expect_error(get_z_correlations("wrong"))
+})
+
+
+
+context("convertStringToVector")
+
+test_that("convertStringToVector with numeric string", {
+    str <- "1.2 1.3 1.4 1.5"
+    expected <- "c(1.2, 1.3, 1.4, 1.5)"
+    expect_equal(convertStringToVector(str), expected)
+})
+
+
