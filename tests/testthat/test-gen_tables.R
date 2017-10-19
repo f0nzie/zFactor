de9cbc6d8379e92be26b5a0d6f0f24140d66436d
fonzie zbook <alfonso.r.reyes@gmail.com>
2017-10-19 15:49:50 -0500

    removing rd file z_dpvr_74p.rda causing a conflict with dpr_short

--- tests/testthat/test-gen_tables.R
+++ tests/testthat/test-gen_tables.R
@@ -1,59 +1,59 @@
 library(testthat)
 
-# context("SK.genDataset7p4t")
-#
-# test_that("SK.genDataset7p4t dimension correct", {
-#     result <- SK.genDataset7p4t()
-#     expect_equal(dim(result), c(4, 7))
-# })
-#
-#
-#
-#
-# context("HY.genDataset7p4t")
-#
-# test_that("HY.genDataset7p4t dimension correct", {
-#     result <- HY.genDataset7p4t()
-#     expect_equal(dim(result), c(4, 7))
-# })
-#
-#
-#
-#
-# context("DAK.genDataset7p4t ")
-#
-# test_that("DAK.genDataset7p4t  dimension correct", {
-#     result <- DAK.genDataset7p4t ()
-#     expect_equal(dim(result), c(4, 7))
-# })
-#
-#
-#
-# context("DPR.genDataset7p4t ")
-#
-# test_that("DPR.genDataset7p4t  dimension correct", {
-#     result <- DPR.genDataset7p4t ()
-#     expect_equal(dim(result), c(4, 7))
-# })
-#
-#
-#
-# context("DPR.genDataset7p4t ")
-#
-# test_that("DPR.genDataset7p4t  dimension correct", {
-#     result <- DPR.genDataset7p4t ()
-#     expect_equal(dim(result), c(4, 7))
-# })
-#
-#
-#
-# context("HY.genDatasetDif ")
-#
-# test_that("HY.genDatasetDif  dimension correct", {
-#     result <- zFactor:::HY.genDatasetDif()
-#     print(result)
-#     expect_equal(dim(result), c(28, 5))
-# })
+context("SK.genDataset7p4t")
+
+test_that("SK.genDataset7p4t dimension correct", {
+    result <- SK.genDataset7p4t()
+    expect_equal(dim(result), c(4, 7))
+})
+
+
+
+
+context("HY.genDataset7p4t")
+
+test_that("HY.genDataset7p4t dimension correct", {
+    result <- HY.genDataset7p4t()
+    expect_equal(dim(result), c(4, 7))
+})
+
+
+
+
+context("DAK.genDataset7p4t ")
+
+test_that("DAK.genDataset7p4t  dimension correct", {
+    result <- DAK.genDataset7p4t ()
+    expect_equal(dim(result), c(4, 7))
+})
+
+
+
+context("DPR.genDataset7p4t ")
+
+test_that("DPR.genDataset7p4t  dimension correct", {
+    result <- DPR.genDataset7p4t ()
+    expect_equal(dim(result), c(4, 7))
+})
+
+
+
+context("DPR.genDataset7p4t ")
+
+test_that("DPR.genDataset7p4t  dimension correct", {
+    result <- DPR.genDataset7p4t ()
+    expect_equal(dim(result), c(4, 7))
+})
+
+
+
+context("HY.genDatasetDif ")
+
+test_that("HY.genDatasetDif  dimension correct", {
+    result <- zFactor:::HY.genDatasetDif()
+    print(result)
+    expect_equal(dim(result), c(28, 5))
+})
 
 
 
